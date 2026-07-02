cdf928a2d314ef86418ecdd8e8b011e6bab9bd04
Nitin Jangid <nitinjangidj92@gmail.com>
2026-07-02 11:48:19 +0530

    abhi

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const http = require('http');
 const handler = require('./api/index.js');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(handler);
 
 server.listen(PORT, () => {
-  console.log(`[server] Development server running at http://localhost:${PORT}`);
-  console.log(`[server] Try accessing: http://localhost:${PORT}/api/jobs`);
+  console.log(`[server] Server listening on port ${PORT}`);
 });
