e3bb353e5594573618d81cdeec35163a00a9657d
Nitin Jangid <nitinjangidj92@gmail.com>
2026-07-02 12:10:39 +0530

    abhi abhi

--- api/index.js
+++ api/index.js
@@ -1,37 +1,37 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Content-Type", "application/json");
 
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
+  if (req.method === "OPTIONS") {
+    res.statusCode = 200;
+    return res.end();
   }
-  const dbPath = path.join(process.cwd(), 'db.json');
-  const db = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
 
+  const dbPath = path.join(process.cwd(), "db.json");
+  const db = JSON.parse(fs.readFileSync(dbPath, "utf8"));
 
-  // Use the URL constructor for robust parsing.
-  // The base URL is required but doesn't matter since we only need the pathname.
-  const url = new URL(req.url, 'http://localhost');
-  // pathname for '/api/jobs/1' will be '/api/jobs/1'
-  const pathSegments = url.pathname.split('/').filter(Boolean); // ['api', 'jobs', '1']
+  const url = new URL(req.url, "http://localhost");
+  const pathSegments = url.pathname.split("/").filter(Boolean);
 
-  const resource = pathSegments[1]; // 'jobs'
-  const id = pathSegments[2];       // '1'
-  res.setHeader('Content-Type', 'application/json');
+  const resource = pathSegments[1];
+  const id = pathSegments[2];
 
   if (db[resource]) {
     if (id) {
-      const item = db[resource].find(item => item.id == id);
-      res.status(200).json(item || { message: 'Not found' });
-    } else {
-      res.status(200).json(db[resource]);
+      const item = db[resource].find((item) => item.id == id);
+      res.statusCode = item ? 200 : 404;
+      return res.end(JSON.stringify(item || { message: "Not found" }));
     }
-  } else {
-    res.status(404).json({ message: 'Resource not found' });
+
+    res.statusCode = 200;
+    return res.end(JSON.stringify(db[resource]));
   }
+
+  res.statusCode = 404;
+  return res.end(JSON.stringify({ message: "Resource not found" }));
 };
